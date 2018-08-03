caff689ac3a1bdfa664682c3e85d4c3fa659afa2
sangnd92 <sangnd@github.com>
2018-08-03 15:46:14 +0700

    init

---
+++ src/main/java/vnext/vn/jenkidemo/JenkidemoApplication.java
@@ -0,0 +1,12 @@
+package vnext.vn.jenkidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkidemoApplication.class, args);
+	}
+}
