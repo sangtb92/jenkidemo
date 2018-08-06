db9d4353e7a822cee52b9611f9250a31bf9a48d8
sangnd92 <sangnd@github.com>
2018-08-06 11:52:23 +0700

    init

---
+++ src/main/java/vnext/vn/jenkidemo/controller/GettingController.java
@@ -0,0 +1,21 @@
+package vnext.vn.jenkidemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import vnext.vn.jenkidemo.models.Getting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GettingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(name = "/getting", method = RequestMethod.GET)
+    public Getting getting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Getting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
